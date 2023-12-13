81ffff4b4972e128efff3c731e29c433560a28f2
Viktor Sandström <sandstrom.viktor@gmail.com>
2023-12-13 10:38:18 +0100

    d5_1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,15 +5,32 @@ fn main() {
   let data = parser("./input");
   let mut seeds = parse_seeds(&data[0].to_owned());
   let conv = parse_conversions(data[1..].to_owned());
-    
+
+  println!("{}", task1(seeds, conv));
+}
+
+fn task1(seeds: Vec<i64>, conv: Vec<Vec<i64>>) -> i64 {
+  let mut transform: Vec<usize> = Vec::with_capacity(seeds.len());
+  for _ in &seeds {
+    transform.push(0);
+  }
+
+  let mut seeds = seeds;
+
   for table in conv {
     for range in table.chunks(3) {
       let ori = range[1]..(range[1] + range[2]);
       for i in 0..seeds.len() {
-        if ori.contains(&seeds[i]) {
-          seeds[i] = range[0] + seeds[i] - range[1];
+        if ori.contains(&seeds[i]) && transform[i] == 0 {
+          let offset = range[0] + seeds[i] - range[1];
+          seeds[i] = offset;
+          transform[i] = 1;
         }
       }
+    }
+
+    for i in 0..transform.len() {
+      transform[i] = 0;
     }
   }
 
@@ -23,8 +40,7 @@ fn main() {
       ans = num;
     }
   }
-  println!("{}", ans);
-
+  ans
 }
 
 fn parse_seeds(data: &String) -> Vec<i64> {
@@ -49,12 +65,7 @@ fn parse_conversions(data: Vec<String>) -> Vec<Vec<i64>> {
         false => None
       }).collect::<Vec<i64>>();
     conv_tables.push(x.clone());
-
-    // for range in x.chunks(3) {
-    //   println!("{}", range[0] - range[1]);
-    // }
   }
-
   conv_tables
 }
 
@@ -90,24 +101,41 @@ mod tests {
   fn result() {
     let data = parser("./test");
     let mut seeds = parse_seeds(&data[0].to_owned());
+    let mut transform: Vec<usize> = Vec::with_capacity(seeds.len());
+    for _ in &seeds {
+      transform.push(0);
+    }
+
     let conv = parse_conversions(data[1..].to_owned());
     for table in conv {
       for range in table.chunks(3) {
         let ori = range[1]..(range[1] + range[2]);
         println!("{:?}", ori);
         for i in 0..seeds.len() {
-          if ori.contains(&seeds[i]) {
+          if ori.contains(&seeds[i]) && transform[i] == 0 {
             let offset = range[0] + seeds[i] - range[1];
             println!("{}", offset);
             seeds[i] = offset;
+            transform[i] = 1;
           }
           println!("{:?}", &seeds);
         }
       }
+      for i in 0..transform.len() {
+        transform[i] = 0;
+      }
     }
     seeds.sort_unstable();
     assert_eq!(35, seeds[0]);
+  }
 
+  #[test]
+  fn task1_test() {
+    let data = parser("./test");
+    let mut seeds = parse_seeds(&data[0].to_owned());
+    let conv = parse_conversions(data[1..].to_owned());
+
+    assert_eq!(35, task1(seeds, conv));
 
   }
 }
